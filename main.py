6e4f395838acaf969550f38b613d849b2ba44193
Auric Vente <mprops@protonmail.com>
2024-05-01 20:19:16 -0600

    Mods

--- main.py
+++ main.py
@@ -8,18 +8,21 @@ ui = UInput(cap, name="virtual-device")
 mouse.grab()
 print("Started")
 
-for event in mouse.read_loop():
-    if event.type == e.EV_KEY:
-        if event.code == 115 and event.value == 1:
-            ui.write(e.EV_KEY, e.KEY_LEFTMETA, 1)
-            ui.write(e.EV_KEY, e.KEY_LEFTCTRL, 1)
-            ui.write(e.EV_KEY, e.KEY_SPACE, 1)
-            ui.syn()
+try:
+    for event in mouse.read_loop():
+        if event.type == e.EV_KEY:
+            if event.code == 115 and event.value == 1:
+                ui.write(e.EV_KEY, e.KEY_LEFTMETA, 1)
+                ui.write(e.EV_KEY, e.KEY_LEFTCTRL, 1)
+                ui.write(e.EV_KEY, e.KEY_SPACE, 1)
+                ui.syn()
 
-            ui.write(e.EV_KEY, e.KEY_SPACE, 0)
-            ui.write(e.EV_KEY, e.KEY_LEFTCTRL, 0)
-            ui.write(e.EV_KEY, e.KEY_LEFTMETA, 0)
-            ui.syn()
-        elif event.code == 114:
-            ui.write(e.EV_KEY, e.KEY_ESC, event.value)
-            ui.syn()
+                ui.write(e.EV_KEY, e.KEY_SPACE, 0)
+                ui.write(e.EV_KEY, e.KEY_LEFTCTRL, 0)
+                ui.write(e.EV_KEY, e.KEY_LEFTMETA, 0)
+                ui.syn()
+            elif event.code == 114:
+                ui.write(e.EV_KEY, e.KEY_ESC, event.value)
+                ui.syn()
+except KeyboardInterrupt:
+    pass
