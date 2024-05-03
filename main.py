6a4ac225435a7d76d77dbb6536fc542f40969051
Auric Vente <mprops@protonmail.com>
2024-05-02 20:29:21 -0600

    Mods

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 import subprocess
 from evdev import InputDevice, list_devices, UInput, ecodes as e
 
-device_name = "BTL Gaming Mouse Consumer Control"
+device_name_1 = "BTL Gaming Mouse"
+device_name_2 = "BTL Gaming Mouse Consumer Control"
+
+def get_input_number(device_name) -> str:
+    devices = subprocess.run("xinput list", shell=True, capture_output=True).stdout.decode().split("\n")
+
+    for device in devices:
+        if device_name in device:
+            return device.split("id=")[-1].split("\t")[0]
+
+    print("Device not found:", device_name)
+    return ""
 
 def get_event_number(device_name) -> str:
-    print("Getting device number...")
     devices = [InputDevice(path) for path in list_devices()]
+
     for device in devices:
         if device.name == device_name:
             return device.path.split("/")[-1]
@@ -13,14 +24,13 @@ def get_event_number(device_name) -> str:
     print("Device not found:", device_name)
     return ""
 
-def start():
-    print(f"Found: {event_number}")
-    mouse_path = f"/dev/input/{event_number}"
+def start_listener(num):
+    print(f"Starting listener ({num})")
+    mouse_path = f"/dev/input/{num}"
     mouse = InputDevice(mouse_path)
     cap = {e.EV_KEY: [e.KEY_ESC, e.KEY_LEFTMETA, e.KEY_LEFTCTRL, e.KEY_SPACE]}
     ui = UInput(cap, name="virtual-device")
     mouse.grab()
-    print("Started")
 
     try:
         for event in mouse.read_loop():
@@ -41,9 +51,19 @@ def start():
     except KeyboardInterrupt:
         pass
 
-event_number = get_event_number(device_name)
+def setup_buttons(num):
+    print(f"Setting up buttons ({num})")
+    cmd = f"xinput set-button-map {num} 3 2 1 4 5 6 7 8 9"
+    subprocess.run(cmd, shell=True)
+
+input_number = get_input_number(device_name_1)
+
+if input_number:
+    setup_buttons(input_number)
+
+event_number = get_event_number(device_name_2)
 
 if event_number:
-    start()
+    start_listener(event_number)
 else:
-    print("Failed to find device.")
+    print("Failed to find device")
